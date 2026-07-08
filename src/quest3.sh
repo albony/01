8d6f91c38c6c5800b79939231ea189bbcbafa073
nnnnnn <nnnnnn.com>
2026-07-08 12:53:46 +0300

    перемещение файлов

---
+++ src/quest3.sh
@@ -0,0 +1,5 @@
+mv door_management_fi door_management_files
+mkdir door_configuration door_map door_logs door_managment_files/
+mv door_map_1.1 door_map
+mv *.log door_logs/
+mv *.conf door_configuration/
